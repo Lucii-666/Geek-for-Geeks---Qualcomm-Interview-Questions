6604dfdb02383001ef7df6c1215b45ad9480154e
Ashutosh Kumar Singh <144926351+Lucii-666@users.noreply.github.com>
2025-08-02 10:37:45 +0530

    Create Find_the_Length_of_Loop.cpp

---
+++ Find_the_Length_of_Loop.cpp
@@ -0,0 +1,35 @@
+/*
+class Node {
+ public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution {
+  public:
+    int lengthOfLoop(Node *head) {
+        Node* s = head;
+        Node* f = head;
+        while (f != NULL && f->next != NULL) {
+            s = s->next;
+            f = f->next->next;
+            
+            if (s == f) {
+                int cnt = 1;
+                Node* temp = s->next;
+                while (temp != s) {
+                    cnt++;
+                    temp = temp->next;
+                }
+                return cnt;
+            }
+        }
+        return 0;
+    }
+};
